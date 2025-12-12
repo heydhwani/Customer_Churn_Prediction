93220f9c33fd6ef36998ce3b16a10880166451bb
Dhwani <dhwanijain731@gmail.com>
2025-12-12 14:57:02 +0530

    applied train test split

--- churn_simple.py
+++ churn_simple.py
@@ -56,3 +56,12 @@ pipeline = Pipeline([
     ("preprocessing", preprocessor),
     ("model", model)
 ])
+
+# TRAIN-TEST SPLIT
+
+X_train, X_test, y_train, y_test = train_test_split(
+    X, y, test_size=0.2, random_state=42, stratify=y
+)
+
+print("\nTrain shape:", X_train.shape)
+print("Test shape:", X_test.shape)
