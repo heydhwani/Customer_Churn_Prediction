d143f72b7f85ae6b0f800a536f4b979b1b6c4ca9
Dhwani <dhwanijain731@gmail.com>
2025-12-12 14:48:49 +0530

    imported libraries

--- churn_simple.py
+++ churn_simple.py
@@ -1,19 +1,10 @@
 import pandas as pd
-import numpy as np
+from pathlib import Path
+import joblib
+
 from sklearn.model_selection import train_test_split
 from sklearn.preprocessing import OneHotEncoder, StandardScaler
 from sklearn.compose import ColumnTransformer
 from sklearn.pipeline import Pipeline
 from sklearn.ensemble import RandomForestClassifier
-from sklearn.linear_model import LogisticRegression
-from sklearn.metrics import accuracy_score, f1_score, roc_auc_score, classification_report, confusion_matrix
-import joblib
-
-df = pd.read_csv("Data/Bank Customer Churn Prediction.csv")
-print("Rows, cols:", df.shape)
-# Quick look
-print(df.head().T)
-
-#Basic cleaning
-if 'customerID' in df.columns:
-    df = df.drop(columns=['customerID'])
+from sklearn.metrics import accuracy_score, classification_report, confusion_matrix
