913bb4818dbea858c0f10a7f2cb19bfbbff8aea3
Dhwani <dhwanijain731@gmail.com>
2025-12-12 14:08:24 +0530

    loaded dataset for cleaning

--- eda_and_cleaning.py
+++ eda_and_cleaning.py
@@ -2,3 +2,11 @@ import pandas as pd
 import seaborn as sns
 import matplotlib.pyplot as plt
 from pathlib import Path
+
+# LOAD DATA
+
+data_path = Path("Data/Bank Customer Churn Prediction.csv")
+df = pd.read_csv(data_path)
+
+print("\n✓ Raw dataset loaded")
+print(df.head())
